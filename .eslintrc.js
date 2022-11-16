e3ac256eb8203297c4d86dc54dad6cd06965f41b
Rafael Santos <rafaelcs_94@hotmail.com>
2022-11-16 15:50:36 -0300

    build: :wrench: adds new dependencies and update config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,5 +11,28 @@ module.exports = {
     sourceType: 'module'
   },
   rules: {
+    'max-len': [
+      'error',
+      100,
+      {
+        ignoreComments: true
+      }
+    ],
+    'max-lines': [
+      'error',
+      250
+    ],
+    'max-lines-per-function': [
+      'error',
+      {
+        max: 20,
+        skipBlankLines: true,
+        skipComments: true
+      }
+    ],
+    'max-params': [
+      'error',
+      4
+    ]
   }
 }
