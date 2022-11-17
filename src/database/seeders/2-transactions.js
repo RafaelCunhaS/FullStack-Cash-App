4ba90ee16b0b5dff1b973c60453151bb891c8177
Rafael Santos <rafaelcs_94@hotmail.com>
2022-11-17 17:19:19 -0300

    build: :wrench: updates transactions seeder

--- src/database/seeders/2-transactions.js
+++ src/database/seeders/2-transactions.js
@@ -11,13 +11,13 @@ module.exports = {
         debited_account_id: 2,
         credited_account_id: 1,
         value: 100,
-        created_at: '2022-11-17 14:19:00.996 +00:00'
+        created_at: '2022-11-18 14:19:00.996 +00:00'
       },
       {
         debited_account_id: 3,
         credited_account_id: 2,
         value: 10,
-        created_at: '2022-11-17 14:19:00.996 +00:00'
+        created_at: '2022-11-19 14:19:00.996 +00:00'
       }
       ])
   },
