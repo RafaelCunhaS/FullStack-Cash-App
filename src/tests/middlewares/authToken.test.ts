97f774c876c1ef8d9830c0d27aa7786053eaa60c
Rafael Santos <rafaelcs_94@hotmail.com>
2022-11-17 09:54:11 -0300

    test: :white_check_mark: adds test for token authorization

---
+++ src/tests/middlewares/authToken.test.ts
@@ -0,0 +1,39 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import { expect } from 'chai';
+import { app } from '../../app'
+
+chai.use(chaiHttp);
+
+describe('Token authorization', () => {
+  describe('If no token is passed through authorization headers', async () => {
+    let response: any
+
+    before( async () => {
+      response = await chai.request(app).get('/account/Joaozinho');
+    });
+
+    it('Should return the status code 401', () => {
+      expect(response).to.have.status(401);
+    });
+    it('The body should have a error message', () => {
+      expect(response.body.message).to.be.equal('Token not found');
+    });
+  });
+
+  describe('If the password have less than 8 characters', async () => {
+    let response: any
+
+    before( async () => {
+      response = await chai.request(app).get('/account/Joaozinho')
+        .set('authorization', 'invalidToken')
+    });
+
+    it('Should return the status code 400', () => {
+      expect(response).to.have.status(401);
+    });
+    it('The body should have a error message', () => {
+      expect(response.body.message).to.be.equal('Token must be a valid token');
+    });
+  });
+});
