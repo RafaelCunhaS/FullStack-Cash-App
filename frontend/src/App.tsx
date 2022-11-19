ce8878011a1500652865c309a2a44e286d9a0579
Rafael Santos <rafaelcs_94@hotmail.com>
2022-11-19 13:01:38 -0300

    feat: :sparkles: sets toastify config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
 import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { AuthProvider } from './hooks/auth';
 import { Routes } from './routes';
 
@@ -7,6 +9,11 @@ function App() {
     <BrowserRouter>
       <AuthProvider>
         <Routes />
+        <ToastContainer
+          position="top-right"
+          autoClose={2500}
+          theme="dark"
+        />
       </AuthProvider>
     </BrowserRouter>
   )
