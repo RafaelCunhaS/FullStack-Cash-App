ff6492a7006d431064db0e3e6affef99f22717b6
Rafael Santos <rafaelcs_94@hotmail.com>
2022-11-19 19:54:23 -0300

    build: :wrench: sets and updates docker files and vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    open: true
+  },
   plugins: [react()]
 })
