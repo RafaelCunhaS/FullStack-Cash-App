76a5c9b7dfdd9e94779eca11d04595670ce59a0b
Rafael Santos <rafaelcs_94@hotmail.com>
2022-11-19 19:16:11 -0300

    feat: :sparkles: adds filters to TransacsionsTable

--- frontend/src/components/TransactionsTable/index.tsx
+++ frontend/src/components/TransactionsTable/index.tsx
@@ -1,13 +1,23 @@
 import { useEffect, useState } from 'react';
 import { ITransactions } from '../../interfaces';
 import { api } from '../../services/api';
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
 
 export function TransactionsTable() {
   const username = localStorage.getItem('username') as string
   const [transactions, setTransactions] = useState<ITransactions[]>([] as ITransactions[])
+  const [startDate, setStartDate] = useState<Date | undefined>(undefined);
+  const [typeToFilter, setTypeToFilter] = useState('');
 
-  async function getTransactions() {
-    const { data } = await api.get('/transaction')
+  async function getTransactions(dateQuery?: string, typeQuery?: string) {
+    let url = '/transaction'
+    if (dateQuery) url += `?date=${dateQuery}`
+    if (typeQuery) {
+      url += url.includes('?') ?`&type=${typeQuery}` : `?type=${typeQuery}`
+    }
+
+    const { data } = await api.get(url)
     setTransactions(data)
   }
 
@@ -15,9 +25,62 @@ export function TransactionsTable() {
     getTransactions()
   }, [])
 
+  function filterTransactions() {
+    const date = startDate ? startDate.toISOString().split("T")[0] : undefined
+    getTransactions(date, typeToFilter)
+  };
+
+  function handleChange(event: any) {
+    setTypeToFilter(event.target.value)
+  }
+
+  function resetRadioState() {
+    setTypeToFilter('')
+    setStartDate(undefined)
+    getTransactions()
+  }
+
   return (
     <div className=".container">
       <h2>Transações</h2>
+      <DatePicker
+        selected={startDate}
+        dateFormat="dd/MM/yyyy"
+        onChange={(date: Date) => setStartDate(date)}
+        maxDate={new Date()}
+      />
+      <label>
+      <input
+        type="radio"
+        value="cashOut"
+        checked={typeToFilter === 'cashOut'}
+        onChange={handleChange}
+      />
+      Enviados
+    </label>
+    <label>
+      <input
+        type="radio"
+        value="cashIn"
+        checked={typeToFilter === 'cashIn'}
+        onChange={handleChange}
+      />
+      Recebidos
+    </label>
+    <div>
+      <button
+        type="reset"
+        onClick={resetRadioState}
+      />
+    </div>
+    <div>
+      <button
+        type="button"
+        onClick={filterTransactions}
+      >
+        Filtrar
+      </button>
+    </div>
       <table>
         <thead>
           <tr>
