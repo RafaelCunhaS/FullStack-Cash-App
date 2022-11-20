dfe23e990a9609db5ee479af4a2d939ca911ff5a
Rafael Santos <rafaelcs_94@hotmail.com>
2022-11-20 13:43:49 -0300

    feat: :lipstick: adds style for Header and Button components

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -2,25 +2,20 @@ import { FiLogOut } from 'react-icons/fi';
 import { GiWallet } from 'react-icons/gi'
 import { useAuth } from '../../hooks/auth';
 import { IHeader } from '../../interfaces';
+import styles from './styles.module.scss'
 
 export function Header({ balance }: IHeader) {
   const { signOut } = useAuth();
   const username = localStorage.getItem('username');
 
   return (
-    <div className=".container">
-      <div className="">
-        <div className="">
-          <div className="">
-            <p className="">
-              Olá, <span>{username}</span>
-            </p>
-            <GiWallet /> <span>{balance}</span>
-            <div className="">
-              <p onClick={signOut}>Sair </p> <FiLogOut />
-            </div>
-          </div>
-        </div>
+    <div className={styles.header}>
+      <p className="">
+        Olá, <span>{username}</span>
+      </p>
+      <p>Saldo<span className={styles.icon}><GiWallet /></span>: {`R$ ${balance}`}</p>
+      <div className={styles.logout}>
+        <p onClick={signOut}>Sair <span className={styles.icon}><FiLogOut /></span></p>
       </div>
     </div>
   );
